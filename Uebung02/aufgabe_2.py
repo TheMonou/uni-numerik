e9c275155d9ce35b3a96207cc117c942e2ad114f
ma6079s <mohammed.alassane@fh-aachen.de>
2025-04-10 21:26:54 +0200

    Überarsch

--- Uebung02/aufgabe_2.py
+++ Uebung02/aufgabe_2.py
@@ -1,15 +1,35 @@
 import numpy as np
+
+
 def gauss(A, b):
-    for i in len(A):
-        pass
+    A = A.astype(float)  # ensure float division
+    b = b.astype(float)
+
+    n = A.shape[0]  # number of rows
+    for k in range(n - 2):
+        faktor = A[k + 1, k] / A[k, k]
+        A[k + 1, :] -= faktor * A[k, :]
+        b[k+1] -= faktor * b[k]
+
+    result = np.zeros_like(b)
+    result[-1] = b[-1]/A[-1,-1]
+    for i in range(n - 2):
+        result[-i] = (b-sum(result[-i+1:-1]))/A[-1,-1]
+    return result
 
 
 def main():
-    pass
+    A = np.array([
+        [1, 3, 1, 1],
+        [0, 1, 0, 2],
+        [2, 1, 0, 0],
+        [0, 4, 4, 0]
+    ])
+
+    b = np.array([8, 7, 1, 12])
+
+    print(gauss(A, b))
 
 
 if __name__ == "__main__":
     main()
-
-
-
